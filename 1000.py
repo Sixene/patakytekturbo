9d05649818ef74229b9403b30459aa9aed83e3b0
Sixene <60512632+Sixene@users.noreply.github.com>
2020-02-01 20:03:53 +0100

    Add files via upload

---
+++ 1000.py
@@ -0,0 +1,24 @@
+import time
+a = input("enter a number as a word between 1 and 20 ")
+if a == "sixteen":
+    print ("I'm Istvan")
+    time.sleep(0.25)
+    print ("and I wanna say")
+    time.sleep(0.25)
+    print ("PATAKYTEKTURBO")
+    time.sleep(0.5)
+    print ("TipTopTiszTiTo")
+    time.sleep(0.5)
+    print ("and of course")
+    time.sleep(0.5)
+    print ("áthúzlak a radiátoron geci")
+    time.sleep(0.5)
+    print ("Greetz to all members of the PatakyLEGO team and Bence Toth and Krisztian Viragh")
+    time.sleep(0.25)
+    print ("you guys are the best")
+    time.sleep(30)
+
+
+
+
+print (a)
